d3370debee40a0feb47345757a3a0a476955d8d2
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-05-06 22:30:57 +0900

    split file for readability

---
+++ ast.go
@@ -0,0 +1,59 @@
+package main
+
+/*
+ * AST
+ */
+
+type node interface {
+	isnode()
+}
+
+type expr interface {
+	node
+	isexpr()
+}
+
+type stmt interface {
+	node
+	isstmt()
+}
+
+// comment does not effect the program.
+type commentStmt struct {
+	stmt
+	message string
+}
+
+// ident = value
+type assignStmt struct {
+	stmt
+	ident *identExpr
+	right expr
+}
+
+type callExpr() struct {
+	expr
+	fnname string
+	args []expr
+}
+
+type identExpr struct {
+	expr
+	name string
+}
+
+type stringExpr struct {
+	expr
+	val string
+}
+
+type int64Expr struct {
+	expr
+	val int64
+}
+
+type float64Expr struct {
+	expr
+	val float64
+}
+
--- parse.go
+++ parse.go
@@ -5,57 +5,6 @@ import (
 	"strconv"
 )
 
-/*
- * AST
- */
-
-type node interface {
-	isnode()
-}
-
-type expr interface {
-	node
-	isexpr()
-}
-
-type stmt interface {
-	node
-	isstmt()
-}
-
-// comment does not effect the program.
-type commentStmt struct {
-	stmt
-	message string
-}
-
-// ident = value
-type assignStmt struct {
-	stmt
-	ident *identExpr
-	right expr
-}
-
-type identExpr struct {
-	expr
-	name string
-}
-
-type stringExpr struct {
-	expr
-	val string
-}
-
-type int64Expr struct {
-	expr
-	val int64
-}
-
-type float64Expr struct {
-	expr
-	val float64
-}
-
 type parser struct {
 	tokens []*token
 	cur    int
