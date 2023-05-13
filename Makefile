452e5b285d4fa68ae21e6956786e1d706791203a
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-05-13 14:15:26 +0900

    use Makefile

---
+++ Makefile
@@ -0,0 +1,18 @@
+BIN = shiba
+
+SRCS = $(shell find . -type f -name '*.go' -print)
+
+$(BIN): $(SRCS) go.mod go.sum
+	go build -o $(BIN) $(SRCS)
+
+.PHONY: format
+format:
+	goimports -w .
+
+.PHONY: test
+test: clean $(BIN)
+	go test ./...
+
+.PHONY: clean
+clean:
+	rm -f $(BIN)
--- e2e_test.go
+++ e2e_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 	"unicode"
@@ -40,11 +41,14 @@ func TestE2E(t *testing.T) {
 			`),
 		},
 	}
+
+	td := t.TempDir()
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			fname := strings.ReplaceAll(name, " ", "_") + ".sb"
+			dfname := filepath.Join(td, fname)
 
-			f, err := os.OpenFile("tests/"+fname, os.O_RDWR|os.O_CREATE, 0755)
+			f, err := os.OpenFile(dfname, os.O_RDWR|os.O_CREATE, 0755)
 			if err != nil {
 				t.Fatalf("create test sb file: %s, err: %s", fname, err)
 			}
@@ -54,7 +58,7 @@ func TestE2E(t *testing.T) {
 				t.Fatalf("write test sb file: %s", fname)
 			}
 
-			result, err := exec.Command("./shiba", "tests/"+fname).CombinedOutput()
+			result, err := exec.Command("./shiba", dfname).CombinedOutput()
 			if err != nil {
 				t.Fatalf("run test sb file (%s): %s\n[%s]", fname, result, err)
 			}
