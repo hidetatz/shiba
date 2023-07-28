54142c8630fb1d6a59ab370f42a3fb7a681808f7
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-07-28 19:23:04 +0900

    support unix syscall as builtin func

--- builtin.go
+++ builtin.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 var bulitinFns = map[string]*obj{
@@ -35,6 +38,61 @@ var bulitinFns = map[string]*obj{
 			}
 
 			return &obj{typ: tI64, ival: int64(target.iterator().size())}, nil
+		},
+	},
+	"syscall": &obj{
+		typ:  tBuiltinFunc,
+		name: "syscall",
+		bfnbody: func(args ...*obj) (*obj, error) {
+			if len(args) != 4 {
+				return NIL, fmt.Errorf("argument mismatch to syscall(): 4 args required")
+			}
+
+			toptr := func(o *obj) (uintptr, error) {
+				switch o.typ {
+				case tI64:
+					return uintptr(o.ival), nil
+				case tStr:
+					// TODO: when this is used as buffer, the sval is not updated via syscall
+					// so tStr must be rewritten.
+					b := append([]byte(o.sval), 0)
+					return uintptr(unsafe.Pointer(&b[0])), nil
+				default:
+					return 0, fmt.Errorf("syscall() arg must be i64 or str")
+				}
+			}
+
+			tr := args[0]
+			if tr.typ != tI64 {
+				return NIL, fmt.Errorf("syscall: first argument must be a syscall number(i64)")
+			}
+
+			trap, err := toptr(tr)
+			if err != nil {
+				return NIL, err
+			}
+
+			a1, err := toptr(args[1])
+			if err != nil {
+				return NIL, err
+			}
+
+			a2, err := toptr(args[2])
+			if err != nil {
+				return NIL, err
+			}
+
+			a3, err := toptr(args[3])
+			if err != nil {
+				return NIL, err
+			}
+
+			r1, r2, errno := unix.Syscall(trap, a1, a2, a3)
+			return &obj{typ: tList, list: []*obj{
+				{typ: tI64, ival: int64(r1)},
+				{typ: tI64, ival: int64(r2)},
+				{typ: tI64, ival: int64(errno)},
+			}}, nil
 		},
 	},
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.20
 require (
 	github.com/MakeNowJust/heredoc/v2 v2.0.1
 	github.com/google/go-cmp v0.5.9
+	golang.org/x/sys v0.10.0
 )
